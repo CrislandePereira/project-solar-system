b2e246a7c0e8af91f93d18926bb5e450918f967f
Crislande Pereira <cris_mg1994@hotmail.com>
2023-05-17 15:20:55 -0300

    Subindo projeto

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,23 @@
+import '../App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    const altName = `Planeta ${planetName}`;
+    return (
+      <div data-testid="planet-card" className="EachPlanet">
+        <img src={ planetImage } alt={ altName } />
+        <p data-testid="planet-name">{planetName}</p>
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
---
+++ src/components/SolarSystem.js
@@ -0,0 +1,24 @@
+import '../App.css';
+import React from 'react';
+import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" className="Planetas">
+        <Title headline="Planetas" />
+        {planets.map((planet) => (
+          <PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
